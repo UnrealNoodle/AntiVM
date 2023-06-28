c1eee15e8d28128452ac6dad55f960d7fceb2ed9
UnrealNoodle <135469960+UnrealNoodle@users.noreply.github.com>
2023-06-28 19:59:03 +0400

    Add files via upload

---
+++ AntiVM.py
@@ -0,0 +1,22 @@
+import os
+import getpass
+
+blacklistUsers = ['WDAGUtilityAccount', '3W1GJT', 'QZSBJVWM', '5ISYH9SH', 'Abby', 'hmarc', 'patex', 'RDhJ0CNFevzX', 'kEecfMwgj', 'Frank', '8Nl0ColNQ5bq', 'Lisa', 'John', 'george', 'PxmdUOpVyx', '8VizSM', 'w0fjuOVmCcP5A', 'lmVwjj9b', 'PqONjHVwexsS', '3u2v9m8', 'Julia', 'HEUeRzl', 'fred', 'server', 'BvJChRPnsxn', 'Harry Johnson', 'SqgFOf3G', 'Lucas', 'mike', 'PateX', 'h7dk1xPr', 'Louise', 'User01', 'test', 'RGzcBUyrznReg']
+
+username = getpass.getuser()
+
+if username.lower() in blacklistUsers:
+    temp_directory = os.path.join(os.environ['TEMP'], 'example_directory')
+    os.makedirs(temp_directory, exist_ok=True)
+
+    filename = "21409323.txt"
+    file_path = os.path.join(temp_directory, filename)
+
+    try:
+        with open(file_path, "w") as file:
+            file.write("")
+        print("Text file created successfully.")
+    except IOError as e:
+        print("An error occurred while writing to the file:", e)
+else:
+    print("PC username not in the blacklist.")
